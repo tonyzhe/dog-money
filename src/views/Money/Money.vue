5b464ff3ab263ab6281ce284e5a5dbb53c67a136
tongZhezhe <9807671+tongzhezhe@user.noreply.gitee.com>
2022-01-20 19:48:11 +0800

    init

---
+++ src/views/Money/Money.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>我是money</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Money extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>
+      我是statistics
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Statistics extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
